4ec670b437046ee28f9f08cfc6fa65213148f966
saeedkhair <saeedkhair1998@gmail.com>
2025-02-25 20:55:19 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -5,12 +5,15 @@ def spread_fire(grid):
     """Update the forest grid based on fire spreading rules."""
     grid_size = len(grid)
     update_grid = copy.deepcopy(grid)
-    for i in range(grid_size-1):
-        for j in range(grid_size-1):
+    new_row = [0] * grid_size
+    grid.append(new_row)
+    for i in range(grid_size):
+        grid[i].append(0)
+
+        for j in range(grid_size):
             if grid[i][j] == 1:
                 neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
                 if 2 in neighbors:
                     update_grid[i][j] = 2
 
     return update_grid
-
